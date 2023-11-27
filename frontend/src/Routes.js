6c6f8405510782b020a4275e901ff8b88dd24aa5
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-11-26 22:48:03 -0500

    Header component - searchbox

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,5 +1,6 @@
 import LoginPage from "./pages/Login.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import ActivationPage from "./pages/ActivationPage.jsx";
+import HomePage from  "./pages/HomePage.jsx";
 
-export { LoginPage, SignUpPage, ActivationPage}
+export { LoginPage, SignUpPage, ActivationPage, HomePage}
---
+++ frontend/src/components/Layout/Header.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import styles from '../../styles/styles';
+import { Link } from "react-router-dom";
+import { productData } from "../../static/data";
+import { AiOutlineSearch } from 'react-icons/ai';
+
+const Header = () => {
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchData, setSearchData] = useState(null);
+
+  const handleSearchChange = (e) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+
+    const filteredProducts = productData.filter((product) => 
+      product.name.toLowerCase().includes(term.toLowerCase())
+    );
+
+    setSearchData(filteredProducts);
+  };
+
+  return (
+    <div className={`${styles.section}`}>
+      <div 
+        className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between"
+      >
+        <div>
+          <Link to="/">
+            <img src="https://shopo.quomodothemes.website/assets/images/logo.svg" alt="ShopO logo" />
+          </Link>
+        </div>
+        <div className='w-[50%] relative'>
+          <input 
+            type="text" 
+            placeholder='Search products' 
+            value={searchTerm} 
+            onChange={handleSearchChange}
+            className='h-[40px] w-full px-2 border-[#3957db] border-[2px] rounded-md'
+          />
+          <AiOutlineSearch 
+            size={30} 
+            className='absolute right-2 top-1.5 cursor-pointer'
+          />
+          {
+            searchData && searchData.length !== 0 ? (
+              <div 
+                className="absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-[9] p-4"
+              >
+                {searchData && searchData.map((i, index) => {
+                  const d = i.name;
+                  const Product_name = d.replace(/\s+/g, "-");
+                  return (
+                    <Link to={`/product/${Product_name}`}>
+                      <div className='w-full flex items-start py-3'>
+                        <img 
+                          src={i.image_Url[0].url}
+                          alt='{i.name}'
+                          className='w-[40px] h-[40px] mr-[10px]'
+                        />
+                        <h1>{i.name}</h1>
+                      </div>
+                    </Link>
+                  )
+                })}
+              </div>
+            ) : ( null )
+          }
+        </div>
+
+        
+      </div>
+    </div>
+  )
+}
+
+export default Header
