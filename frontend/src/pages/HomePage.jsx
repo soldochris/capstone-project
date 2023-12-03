de76ffe29db680a8f50f5cf9bcdc81741fc5b78a
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-12-02 21:50:20 -0500

    Hero component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from "../components/Layout/Header.jsx";
-
+import Hero from "../components/Route/Hero/Hero.jsx";
 
 const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1}/>
+      <Hero />
     </div>
   )
 }
