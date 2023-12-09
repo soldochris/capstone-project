0e4507916d5c8c5b6f0274cdaa3a4900cb93b507
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-12-08 23:06:21 -0500

    Footer component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,6 +6,7 @@ import BestDeals from "../components/Route/BestDeals/BestDeals.jsx";
 import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct.jsx";
 import Events from "../components/Events/Events.jsx";
 import Sponsored from "../components/Route/Sponsored.jsx";
+import Footer from "../components/Layout/Footer.jsx"
 
 const HomePage = () => {
   return (
@@ -17,6 +18,7 @@ const HomePage = () => {
       <Events />
       <FeaturedProduct />
       <Sponsored />
+      <Footer />
     </div>
   )
 }
